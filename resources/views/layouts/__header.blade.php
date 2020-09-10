2cf0ffcec019e94b421e4c077d3f5510a0830dcf
joker <41558684+Mimicircus@users.noreply.github.com>
2020-09-11 03:04:37 +0800

    添加登录后的头像信息，并项目替换掉首页的跳转地址

--- resources/views/layouts/__header.blade.php
+++ resources/views/layouts/__header.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light navbar-static-top">
     <div class="container">
         {{--LOGO--}}
-        <a class="navbar-brand" href="">
+        <a class="navbar-brand" href="{{ url('/') }}">
             Re0
         </a>
         {{--隐藏按钮--}}
@@ -18,8 +18,45 @@
             </ul>
             {{--Right--}}
             <ul class="navbar-nav navbar-right">
-                <li class="nav-item"><a class="nav-link">登录</a> </li>
-                <li class="nav-item"><a class="nav-link">注册</a> </li>
+                @if (Auth::check())
+                    {{--PC--}}
+                    <li class="d-none d-lg-block nav-item dropdown">
+                        <a  class="nav-link dropdown-toggle" href="#" data-toggle="dropdown">
+                            <img src="/uploads/images/avatars/default.jpeg"  class="img-fluid rounded-circle"
+                                 width="40px" height="40px" alt="个人头像">
+                            {{ Auth::user()->name }}
+                        </a>
+                        <div class="dropdown-menu" >
+                            <a class="dropdown-item" href="">个人中心</a>
+                            <a class="dropdown-item" href="">编辑资料</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="#">
+                                <form action="{{ route('logout') }}" method="post" >
+                                    {{ csrf_field() }}
+                                    <button class="btn btn-block btn-danger" type="submit">退出</button>
+                                </form>
+                            </a>
+
+                        </div>
+                    </li>
+                    {{--移动--}}
+                    <li class="d-lg-none nav-item lg text-center"><a class="nav-link">个人中心</a> </li>
+                    <li class="d-lg-none nav-item text-center"><a class="nav-link">编辑资料</a> </li>
+                    <li class="d-lg-none nav-item text-center">
+                        <a class="dropdown-item" href="#">
+                            <form action="{{ route('logout') }}" method="post" >
+                                {{ csrf_field() }}
+                                <button class="btn btn-sm btn-danger" type="submit">退出</button>
+                            </form>
+                        </a>
+                    </li>
+
+
+                @else
+                    <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">登录</a> </li>
+                    <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">注册</a> </li>
+                @endif
+
             </ul>
         </div>
     </div>
--- resources/views/shared/__message.blade.php
+++ resources/views/shared/__message.blade.php
@@ -7,4 +7,4 @@
         </div>
     @endif
 
-@endforeach~
+@endforeach
